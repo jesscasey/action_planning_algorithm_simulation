08662161d64ecae25984eb45028221382e546e9f
Jess Casey <jessicacasey27@yahoo.com>
2024-01-31 18:59:01 +0000

    Add BattleSystem.cs

---
+++ Assets/Scripts/BattleSystem.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class defines the turn-based combat system used in the simulation.
+/// </summary>
+public class BattleSystem : MonoBehaviour
+{
+    // List of possible game states
+    enum BattleState
+    {
+        START,
+        LEFTTURN,
+        RIGHTTURN,
+        END
+    }
+
+    BattleState state;
+
+    // Information about the unit on the left
+    [SerializeField]
+    GameObject leftUnitObject;
+    [SerializeField]
+    Transform leftUnitSpawn;
+    [SerializeField]
+    Unit leftUnit;
+    
+    // Information about the unit on the right
+    [SerializeField]
+    GameObject rightUnitObject;
+    [SerializeField]
+    Transform rightUnitSpawn;
+    [SerializeField]
+    Unit rightUnit;
+
+    /// <summary>
+    /// This method is called as soon as the scene is loaded.
+    /// </summary>
+    void Start()
+    {
+        state = BattleState.START;
+        BattleSetup();
+    }
+
+    /// <summary>
+    /// This method spawns the agents and initialises the game.
+    /// </summary>
+    void BattleSetup()
+    {
+        GameObject leftUnitInfo = Instantiate(leftUnitObject, leftUnitSpawn);
+        leftUnit = leftUnitInfo.GetComponent<Unit>(); // Retrieve unit information
+
+        GameObject rightUnitInfo = Instantiate(rightUnitObject, rightUnitSpawn);
+        rightUnit = rightUnitInfo.GetComponent<Unit>();
+
+        state = BattleState.LEFTTURN;
+    }
+
+    /// <summary>
+    /// This method changes the game's state after a unit has performed an
+    /// action.
+    /// </summary>
+    void ChangeTurn()
+    {
+        state = state == BattleState.LEFTTURN ? BattleState.RIGHTTURN : BattleState.LEFTTURN;
+    }
+
+    /// <summary>
+    /// When called by a unit, this method will reduce the other unit's health
+    /// by a fixed amount.
+    /// </summary>
+    void Attack()
+    {
+        // Determine the unit to be attacked based on whose turn it is
+        Unit enemy = state == BattleState.LEFTTURN ? rightUnit : leftUnit;
+        bool isDead = enemy.TakeDamage();
+
+        if (isDead)
+        {
+            state = BattleState.END;
+        }
+        else
+        {
+            ChangeTurn();
+        }
+    }
+
+    /// <summary>
+    /// This method increases the current unit's health by a fixed amount.
+    /// </summary>
+    void Heal()
+    {
+        Unit currentUnit = state == BattleState.LEFTTURN ? leftUnit : rightUnit;
+        currentUnit.Heal();
+        ChangeTurn();
+    }
+}
